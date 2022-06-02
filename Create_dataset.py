be7cd424dd92ae780ac1f36d73c6b1440ae475de
MHelenaneves <s193743@student.dtu.dk>
2022-06-02 23:51:09 +0200

    update data matrix

--- Create_dataset.py
+++ Create_dataset.py
@@ -17,6 +17,7 @@ from scipy import signal
 
 def get_data_matrix():
     data=[]
+    max_restwindows= get_max_restwindows()
     for ID in range(1,17): 
         obs_windows_all=[]
         print(ID)
@@ -24,19 +25,64 @@ def get_data_matrix():
         if ID==8:
             ID=ID+1
         else:
-            obs_windows_all=[]
-            obs_windows_all=sub_windows_EMG(ID)
-        
-            if np.size(data,0)==0:
-                data = obs_windows_all
+            if ID >=12:
+                obs_windows=[]
+                obs_windows_filled=[]
+                obs_windows=sub_windows_EMG(ID)
+                index=0
+                obs_windows_filled=fill_windows(obs_windows,max_restwindows, index)
+                
             else:
-                data=np.dstack((data,obs_windows_all))
+                obs_windows=[]
+                obs_windows_filled=[]
+                obs_windows=sub_windows_EMG(ID)
+                index=window_dupli_PD(ID)
+                obs_windows_filled=fill_windows(obs_windows,max_restwindows, index)
+                
+                
+            if np.size(data,0)==0:
+                data = obs_windows_filled
+            else:
+                data=np.dstack((data,obs_windows_filled))
         #print(np.size(data,2))
         
     print("done")
     return data
- 
-def get_tremor_labels():
+#%% 
+
+
+def window_dupli_PD(ID):
+    labels=check_gyro_tremor(sub_windows_gyro(ID))
+    bag_label_tremor=bag_label(labels)
+    index=float("nan")
+    if bag_label_tremor==1:
+        i=0
+        while labels[i]==0:
+            i+=1
+        index=i   
+        
+    return index
+
+def fill_windows(obs_windows,max_restwindows, index): #if the ID is from an HC then index=0
+    #obs_windows=sub_windows_EMG(ID)
+    #max_restwindows= get_max_restwindows()
+    if len(obs_windows) < max_restwindows:
+        addition= np.tile(obs_windows[index,:],((int(max_restwindows-len(obs_windows))),1))
+        obs_windows_filled= np.vstack((obs_windows,addition))
+    elif len(obs_windows) == max_restwindows:
+        obs_windows_filled= obs_windows
+            
+    return obs_windows_filled
+
+def bag_label(labels): #labels is an array of 0 and 1, for each PD sub
+    labels=labels.astype(int)
+    bin_count=np.bincount(labels)
+    bag_label_tremor = float("nan")
+    if bin_count[1] >= (round(len(labels)/2)):
+        bag_label_tremor=1
+    return bag_label_tremor
+ #%% 
+def get_tremor_labels(): #gets all the tremor labels(4366)
     labels_tremor_sub=[] #number of tremor labels(positive) of each sub
     labels_all=[]
     for ID in range(1,12): #all PD subjects
@@ -62,16 +108,18 @@ def get_tremor_labels():
     pos_all=labels_all
     return pos_all
 #the negative labels of PD subjects will not be used in the model, only all the negative labels of HC will be considered
+#%%
 
-def get_neg_labels():
-    neg_labels=[]
-    for ID in range(12,17):
-        obs_windows=[]
-        obs_windows=sub_windows_EMG(ID)
-        neg_labels=np.append(neg_labels, np.ones(len(obs_windows)))
+neg_labels=np.zeros(5)
+# def get_neg_labels():
+#     neg_labels=[]
+#     for ID in range(12,17):
+#         #obs_windows=[]
+#         #obs_windows=sub_windows_EMG(ID)
+#         neg_labels=np.append(neg_labels, 0)
         
-        print(ID)
-    return neg_labels
+#         print(ID)
+#     return neg_labels
  #%%          
 def check_gyro_tremor(obs_gyrowindows_all): #for each sub
     labels=np.zeros(len(obs_gyrowindows_all)) #zeros for initializing
@@ -114,7 +162,7 @@ def bandpower(x, fs, fmin, fmax):
 def sub_windows_EMG(ID): #create a matrix of windows of EMG data(each row is a window, and each window is 6000 samples (3s))
 
     window_labels=get_window_labels(ID, 1)
-    max_restwindows= get_max_restwindows()
+    #max_restwindows= get_max_restwindows()
     emg_filtered= get_EMG_sub(ID)[1]
     #print(np.shape(emg_filtered))
     
@@ -141,7 +189,7 @@ def sub_windows_EMG(ID): #create a matrix of windows of EMG data(each row is a w
     # if len(obs_windows) < max_restwindows:
     #     addition= np.tile(obs_windows[0,:],((int(max_restwindows-len(obs_windows))),1))
     #     obs_windows_all= np.vstack((obs_windows,addition))
-    # if len(obs_windows) == max_restwindows:
+    # elif len(obs_windows) == max_restwindows:
     #     obs_windows_all= obs_windows
 
     
@@ -253,16 +301,23 @@ def get_max_restwindows():
     
     return  max_restwindows 
 
+
+
+
+
+
+
+
 #%% Check StandardScaler    
 
-scaler = StandardScaler()
-standardized = scaler.fit_transform(EMG)
-scaler.fit(EMG)
+# scaler = StandardScaler()
+# standardized = scaler.fit_transform(EMG)
+# scaler.fit(EMG)
 
 #%% Save data matrix
 
 # Some test data
-data=get_data_matrix()
+#data=get_data_matrix()
 
 # Specify the filename of the .mat file
 matfile = 'data_matrix.mat'
